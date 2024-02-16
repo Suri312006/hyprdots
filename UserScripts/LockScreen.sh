d7f79d243c5173d635d5526a2217651d07195355
suri312006 <suri312006@gmail.com>
2024-02-16 10:57:58 -0800

    fixed discord thin, removed rainbow borders

--- UserScripts/LockScreen.sh
+++ UserScripts/LockScreen.sh
@@ -5,7 +5,7 @@ CONFIG="$HOME/.config/swaylock/config"
 /home/suri312006/.config/hypr/scripts/MediaCtrl.sh --stop
 pamixer -m
 sleep 0.5s 
-pids=$(ps ax | grep '/usr/share/discord/resources/app.asar' | grep -v grep | awk '{print $1}')
+pids=$(ps ax | grep '/opt/discord/Discord' | awk '{print $1}')
 read -a array <<< "$pids" 
 echo "$array"
 kill $array
